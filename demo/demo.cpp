69c83c734468bddd842c5f9061990c0a8ae627a9
Jack Hunt <jackmileshunt@gmail.com>
2020-02-18 17:14:36 +0000

    Start buildinkg demo app.

--- demo/demo.cpp
+++ demo/demo.cpp
@@ -2,6 +2,17 @@
 
 using namespace GPLib;
 
-int main(int argc, int *argv[]) {
+int main(int argc, char *argv[]) {
+    // Verify a file is provided.
+    if (argc < 1) {
+        std::cout << "USAGE: ./demo <in_file>" << std::endl;
+        return 0;
+    }
+
+    // Load the CSV file.
+    CSVFile<double, int> data;
+    data.readFromDisk(std::string(argv[1]));
+    std::cout << data.getNumRows() << std::endl;
+
     GPRegressor<double> regressor;
 }
--- demo/demo.hpp
+++ demo/demo.hpp
@@ -1,6 +1,13 @@
 #ifndef GPLIB_DEMO
 #define GPLIB_DEMO
 
+#include <string>
+#include <iostream>
+
+#include <CPPUtils/IO/CSVFile.hpp>
+
 #include "../lib/headers/GPRegressor.hpp"
+
+using CPPUtils::IO::CSVFile;
 
 #endif
