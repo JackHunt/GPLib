03d028f166c19b9a28a4992f8bc3fa3cbbed455a
Jack Hunt <jackh@graphcore.ai>
2022-05-18 17:48:56 +0100

    Minor changes

--- demo/demo.cpp
+++ demo/demo.cpp
@@ -4,7 +4,7 @@ using namespace GPLib;
 
 int main(int argc, char *argv[]) {
     // Verify a file is provided.
-    if (argc < 1) {
+    if (argc < 2) {
         std::cout << "USAGE: ./demo <in_file>" << std::endl;
         return 0;
     }
@@ -12,6 +12,7 @@ int main(int argc, char *argv[]) {
     // Load the CSV file.
     CSVFile<double, int> data;
     data.readFromDisk(std::string(argv[1]));
+    std::cout << argv[1] << std::endl;
     std::cout << data.getNumRows() << std::endl;
 
     GPRegressor<double> regressor;
